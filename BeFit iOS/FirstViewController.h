0e53f75d8f21fe3f45c74ead4adbfc67048e694d
Jon Brown <jonbrown2@mac.com>
2014-09-21 14:53:05 -0400

    Initial Commit

---
+++ BeFit iOS/FirstViewController.h
@@ -0,0 +1,15 @@
+//
+//  FirstViewController.h
+//  BeFit iOS
+//
+//  Created by Jon Brown on 9/21/14.
+//  Copyright (c) 2014 Jon Brown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+
+@end
+
---
+++ BeFit iOS/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  BeFit iOS
+//
+//  Created by Jon Brown on 9/21/14.
+//  Copyright (c) 2014 Jon Brown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
