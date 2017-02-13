1d0ef124f06db7deed08cff93e248278fc7d0895
Jon Brown <jonbrown2@mac.com>
2017-02-13 14:13:03 -0500

    Bug fixes

--- BeFit iOS/ViewControllers/FirstViewController.h
+++ BeFit iOS/ViewControllers/FirstViewController.h
@@ -8,21 +8,42 @@
 
 #import <UIKit/UIKit.h>
 #import "PickerTableView.h"
+#import "LifestyleTableView.h"
+#import "GoalTableView.h"
 
-@interface FirstViewController : UIViewController <GenderPickerControllerDelegate, UIActionSheetDelegate, UITextFieldDelegate>
-{
-    IBOutlet UIButton *GenderButt;
-}
+@interface FirstViewController : UIViewController <GenderPickerControllerDelegate, LifePickerControllerDelegate, GoalPickerControllerDelegate, UIActionSheetDelegate, UITextFieldDelegate, UIBarPositioningDelegate, UINavigationBarDelegate>
+
 @property (weak, nonatomic) IBOutlet UITextField *Age;
 @property (weak, nonatomic) IBOutlet UITextField *Weight;
 @property (weak, nonatomic) IBOutlet UITextField *Hfeet;
 @property (weak, nonatomic) IBOutlet UITextField *Hinches;
 @property (weak, nonatomic) IBOutlet UILabel *Total;
 @property (weak, nonatomic) IBOutlet UITextField *Gender;
+@property(nonatomic, strong) IBOutlet UISwitch *MetricSwitch;
+@property (weak, nonatomic) IBOutlet UILabel *inLab;
+@property (weak, nonatomic) IBOutlet UILabel *ftLab;
+@property (weak, nonatomic) IBOutlet UITextField *HCM;
+@property (weak, nonatomic) IBOutlet UILabel *cmLab;
+@property (weak, nonatomic) IBOutlet UILabel *switchLab;
+@property (weak, nonatomic) IBOutlet UILabel *lbs;
+@property (weak, nonatomic) IBOutlet UITextField *weightMet;
+@property (weak, nonatomic) IBOutlet UIButton *CalcButt;
+@property (weak, nonatomic) IBOutlet UIButton *ResetButt;
+@property (weak, nonatomic) IBOutlet UIButton *GenderButt;
+@property (weak, nonatomic) IBOutlet UIButton *LifeButt;
+@property (weak, nonatomic) IBOutlet UIButton *GoalButt;
+@property (weak, nonatomic) IBOutlet UILabel *Recommended;
+@property (weak, nonatomic) IBOutlet UILabel *SliderLab;
+@property (weak, nonatomic) IBOutlet UISlider *Slider;
+@property (weak, nonatomic) IBOutlet UISwitch *GoalSiwtch;
+@property (weak, nonatomic) IBOutlet UILabel *use;
+@property (weak, nonatomic) IBOutlet UINavigationBar *navigationbar;
+
+@property(nonatomic, strong) UITableViewController *LifeTableVC;
 @property(nonatomic, strong) UITableViewController *SimpleTableVC;
 
-
 - (IBAction)Calculate:(id)sender;
+- (IBAction)Reset:(id)sender;
 
 @end
 
