9fad8b7f30aa0e1f5de3e41c2ba89551ed196d76
Andrej Šlegl <andrej.slegl@gmail.com>
2019-10-28 09:29:21 +0100

    Ported to swift: UISnapNav

--- BeFit iOS/ViewControllers/UISnapNav.swift
+++ BeFit iOS/ViewControllers/UISnapNav.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+import UIKit
+
+class UISnapNav: UINavigationController, UINavigationBarDelegate {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let appearance = UINavigationBar.appearance()
+        appearance.setBackgroundImage(UIImage(), for: .any, barMetrics: .default)
+        appearance.shadowImage = UIImage()
+    }
+    
+    func position(for bar: UIBarPositioning) -> UIBarPosition {
+        return .topAttached
+    }
+}
