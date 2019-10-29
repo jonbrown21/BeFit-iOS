cacd39cc46dae60a0ba3e2d72b0f75f7b04b0e83
Andrej Šlegl <andrej.slegl@gmail.com>
2019-10-29 15:45:49 +0100

    Ported to swift: TrackerINavigationBar

---
+++ BeFit iOS/ViewControllers/TrackerINavigationBar.swift
@@ -0,0 +1,16 @@
+//
+//  TrackerINavigationBar.swift
+//  BeFitTracker
+//
+//  Created by Andrej Slegl on 10/29/19.
+//  Copyright © 2019 Jon Brown. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class TrackerINavigationBar: UINavigationBar {
+    override var barPosition: UIBarPosition {
+        return .topAttached
+    }
+}
