7d3a2d22807cdafbadd3a66a76692da34bbf671a
Andrej Šlegl <andrej.slegl@gmail.com>
2019-10-30 13:28:19 +0100

    Improved ImageDownloader so same image is not loading twice.

--- BeFit iOS/ViewControllers/ImageDownloader.swift
+++ BeFit iOS/ViewControllers/ImageDownloader.swift
@@ -9,32 +9,64 @@
 import Foundation
 
 class ImageDownloader {
-    private init() { }
+    typealias ImageCallback = (UIImage?, Error?) -> Void
     
-    private let queue = DispatchQueue(label: "image downloader", qos: .utility, attributes: .concurrent, autoreleaseFrequency: .inherit, target: .global())
+    private class Downloader {
+        let url: URL
+        private var callbacks = [ImageCallback]()
+        
+        init(url: URL) {
+            self.url = url
+        }
+        
+        func download(on queue: DispatchQueue, callback: @escaping ImageCallback) {
+            let wasEmpty = callbacks.isEmpty
+            callbacks.append(callback)
+            
+            guard wasEmpty else {
+                return
+            }
+            
+            queue.async {
+                var image: UIImage?
+                var error: Error?
+                
+                do {
+                    let data = try Data(contentsOf: self.url)
+                    image = UIImage(data: data)
+                    
+                    if image == nil {
+                        throw CustomError.unableToDecodeImage
+                    }
+                } catch let err {
+                    error = err
+                    print("image loading failed for \"\(self.url)\":/n\(err.localizedDescription)")
+                }
+                
+                DispatchQueue.main.async {
+                    var tmp = [ImageCallback]()
+                    swap(&tmp, &self.callbacks)
+                    
+                    for callback in tmp {
+                        callback(image, error)
+                    }
+                }
+            }
+        }
+    }
+    
+    private init() {
+    }
     
     static let shared = ImageDownloader()
     
-    func image(for url: URL, callback: @escaping (UIImage?, Error?) -> Void) {
-        queue.async {
-            var image: UIImage?
-            var error: Error?
-            
-            do {
-                let data = try Data(contentsOf: url)
-                image = UIImage(data: data)
-                
-                if image == nil {
-                    throw CustomError.unableToDecodeImage
-                }
-            } catch let err {
-                error = err
-                print("image loading failed for \"\(url)\":/n\(err.localizedDescription)")
-            }
-            
-            DispatchQueue.main.async {
-                callback(image, error)
-            }
-        }
+    private let queue = DispatchQueue(label: "image downloader", qos: .utility, attributes: .concurrent, autoreleaseFrequency: .inherit, target: .global())
+    private var downloaders = [URL: Downloader]()
+    
+    func image(for url: URL, callback: @escaping ImageCallback) {
+        assert(Thread.isMainThread)
+        let downloader = downloaders[url] ?? Downloader(url: url)
+        downloaders[url] = downloader
+        downloader.download(on: queue, callback: callback)
     }
 }
